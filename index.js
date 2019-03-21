4f36dc525b603f52778fa2f3db4dc63a945a54c4
Chris Gottsacker <atcgottsacker@gmail.com>
2019-03-20 19:59:59 -0500

    convert to arrow functions

--- index.js
+++ index.js
@@ -5,20 +5,26 @@ var app = express()
 
 const PORT = 4000;
 
-function handlelistening() {
-    console.log(`Listening on: http://localhost:${PORT}`);
-}
+const handlelistening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-function handleHome(req, res){
-    console.log('Hi from home');
+// express object sends request and response object to handler function
+const handleHome = (req, res) => {
+    console.log("handleHome");
     res.send("Hello from home");
 }
 
-function handleProfile(req, res) {
-    res.send("Hello from my profile");
+// function handleProfile(req, res) {
+//     res.send("Hello from my profile");
+// }
+const handleProfile = (req, res) => res.send("Hello from my profile");
+
+const betweenHome = (req, res, next) => {
+    console.log("betweenHome");
+    next();
 }
 
-app.get("/", handleHome);
+// app.get("/", handleHome);
+app.get("/", betweenHome, handleHome);
 
 app.get("/profile", handleProfile);
 
